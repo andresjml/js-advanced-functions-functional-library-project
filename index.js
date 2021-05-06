f20db141d8e4baddcce3ea523ecbc917f6ac7f6f
Andres <andresjml@gmail.com>
2021-05-06 15:52:22 -0400

    Completed Assignment(review uniq/flatten)

--- index.js
+++ index.js
@@ -114,15 +114,27 @@ const fi = (function() {
       return newArr
     },
 
+    //taken from solution brand
+    unpack: function(receiver, arr) {
+      for (let val of arr)
+        receiver.push(val)
+    },
 
-    
-    /* flatten: function() {
-      
-    }, */
+    flatten: function(collection, shallow, newArr=[]) {
+      if (!Array.isArray(collection)) return newArr.push(collection)
+      if (shallow) {
+        for (let val of collection)
+          Array.isArray(val) ? this.unpack(newArr, val) : newArr.push(val)
+      } else {
+        for (let val of collection) {
+          this.flatten(val, false, newArr)
+        }
+      }
+      return newArr
+    },
 
-    uniq: function(array, isSorted, callback){
-      let newArr=[]
-      
+    /* uniq: function(array, isSorted, callback){
+      let newArr=[]     
       
       if (!isSorted){
         for (let i = 0; i < array.length; i++){
@@ -149,6 +161,35 @@ const fi = (function() {
         return newArr
       }
     
+    }, */
+    
+    //taken from solution brand
+    uniqSorted: function(collection, iteratee) {
+      const sorted = [collection[0]]
+      for (let idx = 1; idx < collection.length; idx++) {
+        if (sorted[idx-1] !== collection[idx])
+          sorted.push(collection[idx])
+      }
+      return sorted
+    },
+
+    uniq: function(collection, sorted=false, iteratee=false) {
+      if (sorted) {
+        return fi.uniqSorted(collection, iteratee)
+      } else if (!iteratee) {
+        return Array.from(new Set(collection))
+      } else {
+        const modifiedVals = new Set()
+        const uniqVals = new Set()
+        for (let val of collection) {
+          const moddedVal = iteratee(val)
+          if (!modifiedVals.has(moddedVal)) {
+            modifiedVals.add(moddedVal)
+            uniqVals.add(val)
+          }
+        }
+        return Array.from(uniqVals)
+      }
     },
 
     keys: function(object) {
